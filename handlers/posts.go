c4085a8319479f5bc211ac6a5d08de521d529924
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-09-17 22:58:47 -0400

    CRUD for Posts(Final Part)

--- handlers/posts.go
+++ handlers/posts.go
@@ -87,6 +87,7 @@ func GetPostByIdHandler(s server.Server) http.HandlerFunc {
 
 func UpdatePostHandler(s server.Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		params    := mux.Vars(r)
 		tokenString := strings.TrimSpace(r.Header.Get("Authorization")) // reto propuesto:
 		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, // buscar una forma de no repetir tanto el mismo codigo para validar tokens
 			func(token *jwt.Token) (interface{}, error) {
@@ -97,7 +98,6 @@ func UpdatePostHandler(s server.Server) http.HandlerFunc {
 				return
 			}
 			if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
-				params    := mux.Vars(r)
 				var postRequest = UpsertPostRequest{}
 				err := json.NewDecoder(r.Body).Decode(&postRequest); 
 				if err != nil {
@@ -117,6 +117,35 @@ func UpdatePostHandler(s server.Server) http.HandlerFunc {
 				w.Header().Set("Content-Type", "application/json")
 				json.NewEncoder(w).Encode(PostUpdateResponse{
 					Message: "Post Updated",
+				})
+			} else {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+	}
+}
+
+func DeletePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params    := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization")) 
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{},
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(s.Config().JWTSecret), nil
+			})
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+			if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+				err = repository.DeletePost(r.Context(), params["id"], claims.UserId)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(PostUpdateResponse{
+					Message: "Post Deleted",
 				})
 			} else {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
